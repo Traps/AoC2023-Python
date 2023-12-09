d6cd2ee0b615692602177a16854dcdd442d381ca
Traps <trapsnegel@gmail.com>
2023-12-09 15:37:00 +0100

    cd

--- solution/day01b.py
+++ solution/day01b.py
@@ -1,30 +1,42 @@
-import util
 import re
+from typing import Final
 
-from solution.day01a import evaluate_line
+import util
 
+DAY_NUM: Final[int] = 1
+ 
+def evaluate_line(line:str, pattern:re.Pattern) -> int:
+    matches = tuple(pattern.finditer(line))
 
-DAY_NUM: int = 1
+    v0 = matches[0].groups().index('') + 1
+    v1 = matches[-1].groups().index('') + 1
 
+    return v0 * 10 + v1
 
-def digitize_input(_input:str) -> str:
-    digits = ('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
+def parse_input(_input:str) -> list[int]:
+    numbers = ('one', 'two', 'three', 'four', 'five',
+                    'six', 'seven', 'eight', 'nine')
     
-    number_indices = dict()
-    for value, pattern in enumerate(digits, 1):
-        number_indices[str(value)] = {m.span()[0] for m in re.finditer(pattern, _input)}
-    
-    _input = list(_input)
-    for value, index in number_indices.items():
-        for pos in index:
-            _input[pos] = value
-            
-    return ''.join(_input)
+    groups = [f'((?={i}|{num}))' for i,num in enumerate(numbers, 1)]
+    groups.insert(0, r'((?=\n))')
+
+    pattern = re.compile(fr'(?:{"|".join(groups)})')
+
+    matches = pattern.finditer(_input)
+    match_groups = [m.groups().index('') for m in  matches]
 
 
 def solve(_input: str) -> int:
-    return sum(evaluate_line(line) for line in digitize_input(_input).splitlines())
+    numbers = ('one', 'two', 'three', 'four', 'five',
+                    'six', 'seven', 'eight', 'nine')
+    
+    groups = (f'((?={i}|{num}))' for i,num in enumerate(numbers, 1))
+
+    pattern = re.compile(fr'(?:{"|".join(groups)})')
+
+    return sum(evaluate_line(line, pattern) for line in _input.splitlines())
 
 
 if __name__ == '__main__':
+    parse_input(util.get_sample(1,'b'))
     print(f'Day {DAY_NUM:02d}b answer: {solve(util.get_challenge(DAY_NUM))}')
