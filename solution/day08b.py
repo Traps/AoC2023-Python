9e8b7316e542928024073d91ee0288ecac9fb97f
unknown <trapsnegel@gmail.com>
2023-12-11 18:52:58 +0100

    day 8 part two solution and tests

---
+++ solution/day08b.py
@@ -0,0 +1,38 @@
+import itertools
+
+import math
+
+from typing import Iterator
+
+import util
+
+from solution.day08a import parse_network
+
+DAY_NUM: int = 8
+
+def iter_path(turns:str, network:dict, start_node:str) -> Iterator[str]:    
+    yield (current_node := start_node)
+
+    for turn in itertools.cycle(turns):
+        yield (current_node := network[(current_node, turn)])
+    
+
+
+
+def solve(_input: str) -> int:
+    turns, nodes = _input.split('\n\n')
+
+    network = parse_network(nodes)
+
+    start_nodes = {node for node, turn in network if node.endswith('A')}
+
+    iter_from = lambda start: enumerate(iter_path(turns, network, start))
+
+    exit_interval = [next(i for i,n in iter_from(start_node) if n.endswith('Z'))
+                        for start_node in start_nodes]
+    
+    return math.lcm(*exit_interval)
+
+
+if __name__ == '__main__':
+    print(f'Day {DAY_NUM:02d}b answer: {solve(util.get_challenge(DAY_NUM))}')
--- test/test_day08.py
+++ test/test_day08.py
@@ -1,11 +1,17 @@
 import pytest
 import util
 
-from solution import day08a
+from solution import day08a, day08b
 
 def test_day08a_sample():
-    assert day08a.solve(util.get_sample(8, option=0)) == 2
-    assert day08a.solve(util.get_sample(8, option=1)) == 6
+    assert day08a.solve(util.get_sample(8, 'a', option=0)) == 2
+    assert day08a.solve(util.get_sample(8, 'a', option=1)) == 6
 
 def test_day08a_challenge():
     assert day08a.solve(util.get_challenge(8)) == 12643
+
+def test_day08b_sample():
+    assert day08b.solve(util.get_sample(8, 'b')) == 6
+
+def test_day08b_challenge():
+    assert day08b.solve(util.get_challenge(8)) == 13133452426987
