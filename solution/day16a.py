66129fab7623fa3d54f5cdc393d9fef063d9be30
unknown <trapsnegel@gmail.com>
2023-12-24 11:17:41 +0100

    pathfinding now done in two layers, a cached inner layer for non-branching paths, and an outer layer that handles branching paths, a lot faster than before, but I'm sure it could be even better

--- solution/day16a.py
+++ solution/day16a.py
@@ -1,9 +1,10 @@
 import dataclasses
 import functools
+from typing import Callable, Final
 
 import util
 
-DAY_NUM: int = 16
+DAY_NUM: Final[int] = 16
 
 
 @dataclasses.dataclass(slots=True, eq=True, frozen=True)
@@ -19,25 +20,9 @@ class Vec2(object):
 class Beam(object):
     pos: Vec2
     vel: Vec2
-    
-    def propagate(self, obstacle:str) -> 'Beam':
-        match self.vel, obstacle:
-            case _, None:
-                return (Beam(self.pos + self.vel, self.vel),)
-            case Vec2(x=1|-1), '/':
-                return (self.rot90(1),)
-            case Vec2(x=1|-1), '\\':
-                return (self.rot90(-1),)
-            case Vec2(y=1|-1), '/':
-                return (self.rot90(-1),)
-            case Vec2(y=1|-1), '\\':
-                return (self.rot90(1),)
-            case Vec2(x=1|-1), '|':
-                return (self.rot90(1), self.rot90(-1))
-            case Vec2(y=1|-1), '-':
-                return (self.rot90(1), self.rot90(-1))
-        
-        return (Beam(self.pos + self.vel, self.vel),)
+
+    def step(self) -> 'Beam':
+        return Beam(self.pos + self.vel, self.vel)
     
     def rot90(self, ccw_steps:int) -> 'Beam':
         directions = (Vec2(1,0), Vec2(0,-1), Vec2(-1,0), Vec2(0,1))
@@ -46,37 +31,80 @@ class Beam(object):
 
         return Beam(self.pos + new_direction, new_direction)
     
+    def in_bounds(self, board_size:Vec2) -> bool:
+        return 0 <= self.pos.x < board_size.x and 0 <= self.pos.y < board_size.y
 
-def parse_obstacles(_input:str) -> dict:
-    obstacles = {}
+
+def make_cached_beam_function(_input:str) -> \
+        Callable[[Beam], tuple[set[Beam], tuple[Beam, ...]]]:
+   
+    OBSTACLES: Final[dict] = {}
     for j,line in enumerate(_input.splitlines()):
-        obstacles |= {Vec2(i,j):obs for i,obs in enumerate(line) if obs!='.'}
+        OBSTACLES |= {Vec2(i,j):obs for i,obs in enumerate(line) if obs!='.'}
 
-    return obstacles
+    BOARD_SIZE: Final[Vec2] = Vec2(_input.index('\n'), _input.count('\n') + 1)
+    
+    @functools.cache
+    def update_beam(beam:Beam) -> Beam:
+        match OBSTACLES.get(beam.pos, None), beam.vel:
+            case None, _:
+                return (beam.step(),)
+            case '/', Vec2(x=1|-1):
+                return (beam.rot90(1),)
+            case '\\', Vec2(x=1|-1):
+                return (beam.rot90(-1),)
+            case '/', Vec2(y=1|-1):
+                return (beam.rot90(-1),)
+            case '\\', Vec2(y=1|-1):
+                return (beam.rot90(1),)
+            case '|', Vec2(x=1|-1):
+                return (beam.rot90(1), beam.rot90(-1))
+            case '-', Vec2(y=1|-1):
+                return (beam.rot90(1), beam.rot90(-1))
+        
+        return (beam.step(),)
+    
+    @functools.cache
+    def extrapolate_beam(beam:Beam) -> tuple[set[Beam], tuple[Beam, ...]]:
+        if not beam.in_bounds(BOARD_SIZE):
+            return (tuple(), tuple())
+        
+        visited = set()
+        to_visit = (beam,)
+
+        while len(to_visit) == 1 and to_visit[0] not in visited:
+            visited.add(to_visit[0])
+
+            to_visit = tuple(bm for bm in update_beam(to_visit[0])
+                                if bm.in_bounds(BOARD_SIZE))
+
+        return visited, to_visit
+
+    return extrapolate_beam
 
 
-def get_energized_count(obstacles:dict, x_range:range, y_range:range, start_beam:Beam) -> int:
+def count_lit_tiles(start_beam:Beam, extrapolate_beam:callable) -> int:
     visited = set()
     to_visit = [start_beam]
-
+    
     while to_visit:
-        beam = to_visit.pop()
-        visited.add(beam)
+        passed_by, new_to_visit = extrapolate_beam(to_visit.pop())
+        
+        visited |= passed_by
 
-        for new_beam in beam.propagate(obstacles.get(beam.pos, None)):
-            if new_beam not in visited and new_beam.pos.x in x_range and new_beam.pos.y in y_range:
+        for new_beam in new_to_visit:
+            if new_beam not in visited:
                 to_visit.append(new_beam)
 
     return len(set(beam.pos for beam in visited))
 
 
 def solve(_input: str) -> int:
-    x_range = range(0, _input.index('\n'))
-    y_range = range(0, _input.count('\n') + 1)
-    
-    obstacles = parse_obstacles(_input)
-    
-    return get_energized_count(obstacles, x_range, y_range, Beam(Vec2(0,0), Vec2(1,0)))
+    extrapolate_beam = make_cached_beam_function(_input)
+    start_beam = Beam(Vec2(0,0), Vec2(1,0))
+
+    return count_lit_tiles(start_beam, extrapolate_beam)
+
 
 if __name__ == '__main__':
     print(f'Day {DAY_NUM:02d}a answer: {solve(util.get_challenge(DAY_NUM))}')
--- solution/day16b.py
+++ solution/day16b.py
@@ -1,36 +1,33 @@
-import functools
-import itertools
+from typing import Iterator
 
 import util
 
-from solution.day16a import parse_obstacles, get_energized_count, Beam, Vec2
+from solution.day16a import make_cached_beam_function, count_lit_tiles, Beam, Vec2
 
 
 DAY_NUM: int = 16
 
 
+def generate_start_beams(_input:str) -> Iterator[Beam]:
+    board_size = Vec2(_input.index('\n'), _input.count('\n') + 1)
+
+    for x in range(0, board_size.x):
+        yield Beam(Vec2(x, 0), Vec2(0, 1))
+        yield Beam(Vec2(x, board_size.y - 1), Vec2(0, -1))
+
+    for y in range(0, board_size.y):
+        yield Beam(Vec2(0, y), Vec2(1, 0))
+        yield Beam(Vec2(board_size.x -1, y), Vec2(-1, 0))
+
+
 def solve(_input: str) -> int:
-    x_range = range(0, _input.index('\n'))
-    y_range = range(0, _input.count('\n') + 1)
-    
-    obstacles = parse_obstacles(_input)
-    
-    start_beams = itertools.chain(
-            (Beam(Vec2(0, y), Vec2(1, 0)) for y in y_range),
-            (Beam(Vec2(x_range.stop - 1, y), Vec2(-1, 0)) for y in y_range),
-            (Beam(Vec2(x, 0), Vec2(0, 1)) for x in x_range),
-            (Beam(Vec2(x, y_range.stop - 1), Vec2(0, -1)) for x in x_range)
-        )
+    extrapolate_beam = make_cached_beam_function(_input)
+    start_beams = generate_start_beams(_input)
 
-    evaluate_start = functools.partial(
-            get_energized_count, 
-            obstacles=obstacles, 
-            x_range=x_range, 
-            y_range=y_range
-        )
-
-    return max(evaluate_start(start_beam=beam) for beam in start_beams)
+    return max(count_lit_tiles(beam, extrapolate_beam) for beam in start_beams)
 
 
 if __name__ == '__main__':
     print(f'Day {DAY_NUM:02d}b answer: {solve(util.get_challenge(DAY_NUM))}')
+
+    
